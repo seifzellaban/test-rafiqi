a5f83941700ca53b8b4b2648ae7ecdfc568c933f
Omar Ellaban <omar.ellaban00@gmail.com>
2025-06-28 03:40:04 +0300

    fix: add proper text file handling in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -40,4 +40,5 @@ window.addEventListener('message', async (message) => {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  assetsInclude: ['**/*.txt'], // Add support for text files
 }));
